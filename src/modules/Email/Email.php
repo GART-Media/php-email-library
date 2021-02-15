e59d513a9ba19d0c542c4d6fd5016a9eb5b409f7
Dennis Eisenfeld <dennis.eisenfeld@yahoo.de>
2021-02-15 23:35:29 +0100

    refactor email props and add smtp options

--- src/modules/Email/Email.php
+++ src/modules/Email/Email.php
@@ -6,11 +6,12 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use Exception\MissingDataException;
 use Exception\MissingMailBodyException;
+use Exception\MissingMailFromException;
+use Exception\MissingMailToException;
 
 class Email
 {
     private $phpmailer;
-    private $body;
 
     public function __construct(array $data, array $options = null)
     {
@@ -21,6 +22,10 @@ class Email
         }
 
         $this->setEmailProperties($data);
+
+        if ($options) {
+            $this->options = $options;
+        }
     }
 
     public function send()
@@ -32,56 +37,101 @@ class Email
         }
     }
 
+    public function setOptions($options)
+    {
+        foreach ($options as $option => $value) {
+            switch ($option) {
+                case "smtp":
+                    $this->setSMTPSettings($value["settings"]);
+                    break;
+
+                case "html":
+                    $this->phpmailer->isHTML($value);
+                    break;
+            }
+        }
+    }
+
     private function setEmailProperties(array $data)
     {
-        $this->setRecipients($data["to"]);
-        $this->setSender($data["from"]);
-        $this->setCC($data["cc"]);
-        $this->setBCC($data["bcc"]);
-
-        //TODO: split body props 
-        if ($data["body"]["subject"]) {
-            foreach ($data["body"] as $key => $value) {
-                $this->body[$key] = $value;
-            }
+        if ($data["to"]) {
+            $this->setRecipients($data["to"]);
         } else {
-            throw new MissingMailBodyException("No mail body supplied to class constructor.");
+            throw new MissingMailToException("No recipient data was supplied to class constructor!\nMissing key: 'to'");
+        }
+
+        if ($data["from"]) {
+            $this->setSender($data["from"]);
+        } else {
+            throw new MissingMailFromException("No sender data was supplied to class constructor!\nMissing key: 'from'");
+        }
+
+        if ($data["cc"]) {
+            $this->setCC($data["cc"]);
+        }
+
+        if ($data["bcc"]) {
+            $this->setBCC($data["bcc"]);
+        }
+
+        if ($data["body"]) {
+            $this->setBody($data["body"]);
+        } else {
+            throw new MissingMailBodyException("No body data was supplied to class constructor!\nMissing key: 'body'");
         }
     }
 
     private function setSender($from)
     {
-        if ($from) {
-            $this->phpmailer->setFrom(
-                $from["address"],
-                $from["name"]
-            );
-        }
+        $this->phpmailer->setFrom(
+            $from["address"],
+            $from["name"]
+        );
     }
 
     private function setRecipients($to)
     {
-        if ($to) {
-            foreach ($to as $recipient) {
-                $this->phpmailer->addAddress(
-                    $recipient["address"],
-                    $recipient["name"]
-                );
-            }
+        foreach ($to as $recipient) {
+            $this->phpmailer->addAddress(
+                $recipient["address"],
+                $recipient["name"]
+            );
         }
     }
 
     private function setCC($cc)
     {
-        if ($cc) {
-            $this->phpmailer->addCC($cc);
-        }
+        $this->phpmailer->addCC($cc);
     }
 
     private function setBCC($bcc)
     {
-        if ($bcc) {
-            $this->phpmailer->addBCC($bcc);
+        $this->phpmailer->addBCC($bcc);
+    }
+
+    private function setBody($body)
+    {
+        foreach ($body["body"] as $key => $value) {
+            if ($key !== "subject") {
+                $this->phpmailer->body .= $value . "<br>";
+            } else {
+                $this->phpmailer->subject = $value;
+            }
+        }
+    }
+
+    private function setSMTPSettings($settings)
+    {
+        if ($settings["isSmtp"]) {
+            $this->phpmailer->isSMTP();
+            $this->phpmailer->Host = $settings["settings"]["host"];
+            $this->phpmailer->SMTPAuth = $settings["settings"]["auth"];
+            $this->phpmailer->Username = $settings["settings"]["username"];
+            $this->phpmailer->Password = $settings["settings"]["password"];
+            $this->phpmailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $this->phpmailer->Port = $settings["settings"]["port"];
+        } else {
+            $this->phpmailer->isMail();
         }
     }
 }
